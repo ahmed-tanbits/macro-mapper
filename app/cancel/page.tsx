a2d7adca7de29e2c9a230cb092fe7ffceebc2cb4
hassanawan2211 <ali.hassan@tanbits.org>
2025-03-06 12:10:19 +0500

    add caancel screen page navigate to home screen on back button

--- app/cancel/page.tsx
+++ app/cancel/page.tsx
@@ -1,45 +1,14 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import Spinner from "../components/Spinner";
 
 export default function CancelPage() {
     const router = useRouter();
-    const [redirectCountdown, setRedirectCountdown] = useState(3);
-    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const countdownInterval = setInterval(() => {
-            setRedirectCountdown((prev) => {
-                if (prev <= 1) {
-                    clearInterval(countdownInterval);
-                    setLoading(false); // Hide spinner when countdown reaches 1
-                    return 0;
-                }
-                return prev - 1;
-            });
-        }, 1000);
+        router.push("/");
+    }, [router]);
 
-        const timeout = setTimeout(() => router.push("/"), 3000);
-
-        return () => {
-            clearInterval(countdownInterval);
-            clearTimeout(timeout);
-        };
-    }, []);
-
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-6">
-            <div className="bg-white shadow-md rounded-2xl p-6 w-full max-w-md text-center">
-                {loading && <Spinner />} {/* Show spinner only if loading is true */}
-                <h1 className="text-xl font-semibold text-gray-800">
-                    ❌ Subscription canceled. Redirecting to home...
-                </h1>
-                <p className="text-gray-600 mt-2">
-                    Redirecting in {redirectCountdown}...
-                </p>
-            </div>
-        </div>
-    );
+    return null;
 }
